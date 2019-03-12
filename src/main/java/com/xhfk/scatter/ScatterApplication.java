f55d48580431975acd3c0e1da752e15b674b77b7
liujiahai <liujiahai@tunicorn.cn>
2019-03-12 22:43:03 +0800

    Initial commit

---
+++ src/main/java/com/xhfk/scatter/ScatterApplication.java
@@ -0,0 +1,13 @@
+package com.xhfk.scatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScatterApplication.class, args);
+	}
+
+}
